e3f888154e43c10012befa9d04b4054fe2eb5132
IshankaCharuka <135942692+IshankaCharuka@users.noreply.github.com>
2023-07-27 09:11:01 +0530

    Bank

---
+++ BankAccount/src/main/java/com/mycompany/bankaccount/BankAccount.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bankaccount;
+
+/**
+ *
+ * @author ishankacharuka
+ */
+public class BankAccount {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
